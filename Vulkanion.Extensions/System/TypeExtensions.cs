39f8f31e8da70e0dccc714ef8f620c58bfc40a56
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-10-21 13:32:37 +0200

    Added TypeExtensions.

---
+++ Vulkanion.Extensions/System/TypeExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Vulkanion.Extensions.System
+{
+    internal static class TypeExtensions
+    {
+        internal static Type GetElementTypeForExpression(this Type seqType)
+        {
+            var enumerableType = FindIEnumerableType(seqType);
+            return enumerableType == null ? seqType : enumerableType.GetGenericArguments()[0];
+        }
+        internal static MethodInfo GetGenericMethod(this Type owner, string methodName)
+        {
+            return owner.GetMethods().SingleOrDefault(m => m.Name == methodName && m.IsGenericMethod);
+        }
+        private static Type FindIEnumerableType(Type seqType)
+        {
+            if(seqType == null || seqType == typeof(string))
+                return null;
+
+            if(seqType.IsArray)
+                return typeof(IEnumerable<>).MakeGenericType(seqType.GetElementType());
+
+            if(seqType.IsGenericType)
+            {
+                foreach(Type arg in seqType.GetGenericArguments())
+                {
+                    Type ienum = typeof(IEnumerable<>).MakeGenericType(arg);
+                    if(ienum.IsAssignableFrom(seqType))
+                    {
+                        return ienum;
+                    }
+                }
+            }
+
+            var ifaces = seqType.GetInterfaces();
+            if(ifaces?.Length > 0)
+            {
+                foreach(Type iface in ifaces)
+                {
+                    Type ienum = FindIEnumerableType(iface);
+                    if(ienum != null)
+                    {
+                        return ienum;
+                    }
+                }
+            }
+
+            if(seqType?.BaseType != typeof(object))
+            {
+                return FindIEnumerableType(seqType.BaseType);
+            }
+
+            return null;
+        }
+    }
+}
+// From: https://github.com/conwid/BetterRepository, https://dotnetfalcon.com/implementing-the-repository-pattern-with-direct-iqueryable-support/.
